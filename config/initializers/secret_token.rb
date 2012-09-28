23055fede9f81e7581f47e6ba1db55043fa4ce48
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-09-28 15:43:15 +0800

    cn.jobs init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CnJobs::Application.config.secret_token = 'b5db5b5541188a5cb3b06c03bb0d1b8789a42a2671c3cb1a1f3acd9969b0a2ab11711aadd9c91e209f5b7a2a39ba0c40d686bffcfa9287e83a3d803028fa0044'
